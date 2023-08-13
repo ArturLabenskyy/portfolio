2336c66409f094a0014b613073bca3a3930cd7c7
Artur Labenskyy <Labenskyy@gmail.com>
2023-08-13 12:58:45 +0300

    dynamic array generetion done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,12 +2,7 @@ import React from "react";
 
 import "./portfolio.css";
 
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+import { data } from "./data";
 
 const Portfolio = () => {
     return (
@@ -16,160 +11,32 @@ const Portfolio = () => {
             <h2>Portfolio</h2>
 
             <div className="container portfolio-container">
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG1} alt="image 1" />
-                    </div>
-                    <h3>Vanilla JS MineCraft 2D</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/ArturLabenskyy/Weekend-6_Minecraft"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://minecrafting.netlify.app"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG2} alt="image 1" />
-                    </div>
-                    <h3>World Population</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/ArturLabenskyy/Weekend-7-World-Population"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://w0rld-population.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG3} alt="image 1" />
-                    </div>
-                    <h3>Moviliot Project</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/ArturLabenskyy/Middle-Project_Moviliot"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://moviliot.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG4} alt="image 1" />
-                    </div>
-                    <h3>Voting for House Project</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/ArturLabenskyy/Weekend-8_WA_Voting"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://vote-for-house.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG5} alt="image 1" />
-                    </div>
-                    <h3>Hackaton Project</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/SuperSpawn/Hackaton-frontend"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://appleseeds-reddit-possitive-scrapper.netlify.app"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG6} alt="image 1" />
-                    </div>
-                    <h3>iWannaPlay Project</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/ArturLabenskyy/Bootcamp-Final-Frontend"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="https://iwannaplay.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={IMG6} alt="image 1" />
-                    </div>
-                    <h3>Harmony Project</h3>
-                    <div className="portfolio-item-cta">
-                        <a
-                            href="https://github.com/Makes-Innovation-Hub/harmony"
-                            className="btn"
-                            target="_black"
-                        >
-                            Github
-                        </a>
-                        <a
-                            href="http://harmonymusic.tech/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Live Demo
-                        </a>
-                    </div>
-                </article>
+                {data.map(({ id, image, title, github, demo }) => {
+                    return (
+                        <article key={id} className="portfolio-item">
+                            <div className="portfolio-item-image">
+                                <img src={image} alt="image 1" />
+                            </div>
+                            <h3>{title}</h3>
+                            <div className="portfolio-item-cta">
+                                <a
+                                    href={github}
+                                    className="btn"
+                                    target="_black"
+                                >
+                                    Github
+                                </a>
+                                <a
+                                    href={demo}
+                                    className="btn btn-primary"
+                                    target="_blank"
+                                >
+                                    Live Demo
+                                </a>
+                            </div>
+                        </article>
+                    );
+                })}
             </div>
         </section>
     );
---
+++ src/components/Portfolio/data.js
@@ -0,0 +1,83 @@
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+export const data = [
+    {
+        id: 7,
+        image: IMG1,
+        title: "Harmony Project",
+        github: "https://github.com/Makes-Innovation-Hub/harmony",
+        demo: "http://harmonymusic.tech/",
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: "iWannaPlay Project",
+        github: "https://github.com/ArturLabenskyy/Bootcamp-Final-Frontend",
+        demo: "https://iwannaplay.netlify.app/",
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: "Hackaton Project",
+        github: "https://github.com/SuperSpawn/Hackaton-frontend",
+        demo: "https://appleseeds-reddit-possitive-scrapper.netlify.app",
+    },
+    {
+        id: 1,
+        image: IMG1,
+        title: "Vanilla JS MineCraft 2D",
+        github: "https://github.com/ArturLabenskyy/Weekend-6_Minecraft",
+        demo: "https://minecrafting.netlify.app",
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: "World Population",
+        github: "https://github.com/ArturLabenskyy/Weekend-7-World-Population",
+        demo: "https://w0rld-population.netlify.app/",
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: "Moviliot Project",
+        github: "https://github.com/ArturLabenskyy/Middle-Project_Moviliot",
+        demo: "https://moviliot.netlify.app/",
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: "Voting for House Project",
+        github: "https://github.com/ArturLabenskyy/Weekend-8_WA_Voting",
+        demo: "https://vote-for-house.netlify.app/",
+    },
+];
+
+{
+    /* <article className="portfolio-item">
+<div className="portfolio-item-image">
+    <img src={IMG1} alt="image 1" />
+</div>
+<h3>Vanilla JS MineCraft 2D</h3>
+<div className="portfolio-item-cta">
+    <a
+        href="https://github.com/ArturLabenskyy/Weekend-6_Minecraft"
+        className="btn"
+        target="_black"
+    >
+        Github
+    </a>
+    <a
+        href="https://minecrafting.netlify.app"
+        className="btn btn-primary"
+        target="_blank"
+    >
+        Live Demo
+    </a>
+</div>
+</article> */
+}
