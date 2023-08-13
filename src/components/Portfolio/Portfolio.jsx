9fcdda56936ef33bc1fc25e242dc1a87311428df
Artur Labenskyy <Labenskyy@gmail.com>
2023-08-13 12:48:10 +0300

    portfolio links update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,17 +20,17 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG1} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Vanilla JS MineCraft 2D</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/ArturLabenskyy/Weekend-6_Minecraft"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://minecrafting.netlify.app"
                             className="btn btn-primary"
                             target="_blank"
                         >
@@ -42,17 +42,17 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG2} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>World Population</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/ArturLabenskyy/Weekend-7-World-Population"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://w0rld-population.netlify.app/"
                             className="btn btn-primary"
                             target="_blank"
                         >
@@ -64,17 +64,17 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG3} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Moviliot Project</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/ArturLabenskyy/Middle-Project_Moviliot"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://moviliot.netlify.app/"
                             className="btn btn-primary"
                             target="_blank"
                         >
@@ -86,17 +86,17 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG4} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Voting for House Project</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/ArturLabenskyy/Weekend-8_WA_Voting"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://vote-for-house.netlify.app/"
                             className="btn btn-primary"
                             target="_blank"
                         >
@@ -108,17 +108,17 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG5} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Hackaton Project</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/SuperSpawn/Hackaton-frontend"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://appleseeds-reddit-possitive-scrapper.netlify.app"
                             className="btn btn-primary"
                             target="_blank"
                         >
@@ -130,17 +130,39 @@ const Portfolio = () => {
                     <div className="portfolio-item-image">
                         <img src={IMG6} alt="image 1" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>iWannaPlay Project</h3>
                     <div className="portfolio-item-cta">
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://github.com/ArturLabenskyy/Bootcamp-Final-Frontend"
                             className="btn"
                             target="_black"
                         >
                             Github
                         </a>
                         <a
-                            href="https://github.com/ArturLabenskyy"
+                            href="https://iwannaplay.netlify.app/"
+                            className="btn btn-primary"
+                            target="_blank"
+                        >
+                            Live Demo
+                        </a>
+                    </div>
+                </article>
+                <article className="portfolio-item">
+                    <div className="portfolio-item-image">
+                        <img src={IMG6} alt="image 1" />
+                    </div>
+                    <h3>Harmony Project</h3>
+                    <div className="portfolio-item-cta">
+                        <a
+                            href="https://github.com/Makes-Innovation-Hub/harmony"
+                            className="btn"
+                            target="_black"
+                        >
+                            Github
+                        </a>
+                        <a
+                            href="http://harmonymusic.tech/"
                             className="btn btn-primary"
                             target="_blank"
                         >
